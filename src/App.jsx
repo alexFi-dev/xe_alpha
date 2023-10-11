8208c0c21aa4d413609351d254bffef214e2f8a8
Alexeev Vadim <alexeev.vadim@challenge.day>
2023-10-12 04:51:08 +0700

    first commit
    uploaded alpha version of exchanger react App

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import { Box, Container, Grid, Typography } from '@mui/material'
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
+import InputAmount from './components/InputAmount'
+import SelectCountry from './components/SelectCountry'
+import SwitchCurrency from './components/SwitchCurrency'
+import { CurrencyContext } from './context/CurrencyContext'
+
+function App() {
+  const {
+    fromCurrency,
+    setFromCurrency,
+    toCurrency,
+    setToCurrency,
+    firstAmount,
+    setFirstAmount
+  } = useContext(CurrencyContext)
+  const [resultCurrency, setResultCurrency] = useState(0);
+  const codeFromCurrency = fromCurrency.split(" ")[1];
+  const codeToCurrency = toCurrency.split(" ")[1];
+
+  useEffect(() => {
+    if(firstAmount) {
+      axios("https://api.freecurrencyapi.com/v1/latest", {
+        params: {
+          apikey: "fca_live_R959j1sRGJNIQMCTgK6znRLVtHBHBFt1pxU16V0z",
+          base_currency: codeFromCurrency,
+          currencies: codeToCurrency
+        }
+      })
+        .then(response => setResultCurrency(response.data.data[codeToCurrency]))
+        .catch(error => console.log(error))
+    }
+  }, [firstAmount, fromCurrency, toCurrency])
+
+  const boxStyles = {
+    background: "#fdfdfd",
+    margin: "2rem 0",
+    textAlign: "center",
+    color: "#222",
+    padding: "2rem",
+    borderRadius: 2,
+    boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)",
+    position: "relative"
+  }
+
+  return (
+    <Container maxWidth="md" sx={boxStyles}>
+      <Typography variant='h6' sx={{margin: "20px 0"}}>Exchange bot. Alpha v.0.1</Typography>
+      <Grid container spacing={2}>
+        <InputAmount />
+        <SelectCountry value={fromCurrency} setValue={setFromCurrency} label="From" />
+        <SwitchCurrency />
+        <SelectCountry value={toCurrency} setValue={setToCurrency} label="To" />
+      </Grid>
+      {firstAmount ? (
+        <Box sx={{ textAlign: "left", marginTop: "2rem"}}>
+          <Typography>{firstAmount} {fromCurrency} =</Typography>
+          <Typography variant='h5' sx={{ marginTop: "5px", fontWeight: "bold"}}> {Math.round(resultCurrency*firstAmount * 100) / 100} {toCurrency} </Typography>
+        </Box>
+      ) : ""}
+    </Container>
+  )
+}
+
+export default App
